907f214fdbfc46dd486706f27af2cfe9b0c063b1
Zarha <zarhaselene@gmail.com>
2024-11-12 10:23:41 +0100

    Implement asynchronous data fetching and dynamic content rendering for accordion

--- code/script.js
+++ code/script.js
@@ -4,6 +4,29 @@ function toggle(e) {
   element.classList.toggle("active");
 }
 
-// Selects and HTML element, and calls a function which will be executed when the element is clicked.
-const section1Element = document.getElementById("section1");
-section1Element.addEventListener("click", toggle);
+const container = document.querySelector(".accordion");
+
+async function getPost() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+
+  data.forEach((element) => {
+    const title = document.createElement("div");
+    title.className = "title";
+    title.innerHTML = `${element.title}`;
+
+    const description = document.createElement("div");
+    description.className = "description";
+    description.innerHTML = `<p>${element.body}</p>`;
+
+    container.appendChild(title);
+    container.appendChild(description);
+
+    // Selects and HTML element, and calls a function which will be executed when the element is clicked.
+    const titleElement = document.querySelectorAll(".title");
+    titleElement.forEach((element) => {
+      element.addEventListener("click", toggle);
+    });
+  });
+}
+getPost();
